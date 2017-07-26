c8c945211b0bb5b346d4a3655e9fdd2fa52006e5
imjcham <imjcham@github.com>
2017-07-26 14:53:30 -0700

    add more logging

--- app.py
+++ app.py
@@ -5,37 +5,77 @@ import json
 import googleapiclient.discovery
 import io
 import httplib2
-import logging
+import urllib
 
 from PIL import Image
-from urllib import urlopen
+from urllib import request, parse
+from urllib.request import urlopen
 from flask import Flask, render_template, request
+from google.cloud import logging
 
 app = Flask(__name__)
+client = logging.Client()
+logger = client.logger("mlhotdog")
 
 @app.route("/", methods=["GET"])
 def home():
   return render_template("index.html")
 
+@app.route("/tasks/keepmlalive")
+def keep_ml_alive():
+  logger.log_struct({
+      "task": "keepmlalive",
+      "urlRoot": request.url_root,
+  })
+
+  data = json.dumps({"fileUrl": "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Hot_dog_with_mustard.png/1200px-Hot_dog_with_mustard.png"}).encode()
+  req = urllib.request.Request(request.url_root + "api/predict", data=data, headers={"content-type": "application/json"})
+  resp = urllib.request.urlopen(req)
+  data = resp.read()
+  return "DONE"
+
 @app.route("/api/predict", methods=["POST"])
 def predict():
-  payload = json.loads(request.data.decode("utf-8"))
+  print("Start of request")
+  if request.data is None or not request.data.decode("utf-8"):
+    logger.log_struct({
+        "error": "no request body",
+        "request": request}, severity="ERROR")
+    return json.dumps({"error": "no request body"}), 400
+
+  print("Parsing payload as json")
+  try:
+    payload = json.loads(request.data.decode("utf-8"))
+  except:
+    logger.log_struct({
+        "error": "invalid json post payload",
+        "exception": sys.exc_info()}, severity="ERROR")
+    return json.dumps({"error": "invalid json post payload"}), 400
+
+  print("Getting image")
   img = None
   if "fileUrl" in payload:
-    img = base64.b64encode(urlopen(payload.get("fileUrl")).read())
+    try:
+      img = base64.b64encode(urlopen(payload.get("fileUrl")).read())
+    except:
+      logger.log_text({"error": "failed to fetch image", "url": payload.get("fileUrl"), "exception": sys.exc_info()})
+      return json.dumps({"error": "failed to fetch image", "url": payload.get("fileUrl")}), 500
   elif "fileBinary" in payload:
     img = payload.get("fileBinary", None).split(",")[1]
   else:
+    logger.log_struct({"error": "no image"}, severity="ERROR")
     return json.dumps({"error": "no image"}), 400
 
   # Read in image as an Image in memory
+  print("Reading image in memory")
   try:
     buff = Image.open(io.BytesIO(base64.b64decode(img)))
   except:
-    logging.error(sys.exc_info())
+    logger.log_struct({"error": "not given valid image", "exception": sys.exc_info()}, severity="ERROR")
     return json.dumps({"error": "Not given a valid image"}), 400
 
   # Resize image to be at max 1024x1024
+  print("Resizing image")
   try:
     size = 1024, 1024
     buff.thumbnail(size, Image.ANTIALIAS)
@@ -49,13 +89,14 @@ def predict():
     img = base64.b64encode((out_buff.getvalue())).decode("ascii")
 
   except:
-    logging.error(sys.exc_info())
+    logger.log_struct({"error": "Could not manipulate image to be a jpeg of 1024x1024", "exception": sys.exc_info()}, severity="ERROR")
     return json.dumps({"error": "Could not manipulate image to be a jpeg of 1024x1024"}), 500
 
+  print("Starting to call ml service")
   try:
     service = googleapiclient.discovery.build("ml", "v1")
     resp = service.projects().predict(
-        name="projects/jcham-1469824226729/models/hotdogmodel/versions/beta4",
+        name="projects/jcham-1469824226729/models/hotdog",
         body={
             "instances": [{
                 "key": "0",
@@ -64,9 +105,9 @@ def predict():
                 },
             }]
         }).execute()
-    logging.info(resp)
+    logger.log_struct({"predictionResp": resp}, severity="DEBUG")
   except:
-    logging.error(sys.exc_info())
+    logger.log_struct({"error": "Failed to get ML Prediction", "exception": sys.exc_info()}, severity="ERROR")
     return json.dumps({"error": "Failed to get ML prediction"}), 500
 
   return json.dumps(resp)
